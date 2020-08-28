5f3af4adce116420bacace47faf3f0fb7e5c9354
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-28 17:06:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
 def list_dwarves(array)
   array.each_with_index do |name, index|
     index_plus = array[index] += 1
-    puts "#{index}. #{name}"
+    puts "#{index_plus}. #{name}"
   end
 end
   # Use `each_with_index` to enumerate over the provided array
