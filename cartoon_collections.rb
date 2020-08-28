e123ab5cf2c6235bf0657729f0ff878ff522db3c
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-28 16:38:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,15 @@
 def greet_characters(array)
+  array.each |name|
+  print "Hello #{name}!"
+end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
+
 
 def list_dwarves(array)
+  array.each_with_index
+end
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-end
