b670a1edba4604c37e24e6ea9edd018b977be0fc
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-28 16:40:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def greet_characters(array)
   array.each do |name|
   puts "Hello #{name}!"
 end
+end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
